4ede7186b659c44baf1666b10c4b79d1bea71fe5
Erik Jagre <erik.jagre@educ.goteborg.se>
2023-03-18 09:52:58 +0100

    added function for bus write

--- SpiSlave/SpiSlave_tb.vhd
+++ SpiSlave/SpiSlave_tb.vhd
@@ -12,21 +12,35 @@ architecture tb of spi_slave_tb is
 signal spi_clk, spi_mosi : std_logic := '0';
 signal dta : std_logic_vector(7 downto 0) := X"81";
 
+
+procedure bus_write(
+   data : in  std_logic_vector(7 downto 0);
+   signal sclk : out std_logic;
+   signal mosi : out std_logic)
+is
+   variable dta_v : std_logic_vector(7 downto 0);
+begin
+   dta_v := data;
+   for i in 0 to 7 loop
+      mosi <= dta_v(7);
+      dta_v := dta_V(6 downto 0) & '0';
+      wait for 1 us;
+      sclk <= '1';
+      wait for 1 us;
+      sclk <= '0';
+   end loop;
+end;
+
 begin
   --*COMBINATORIAL LGOIC*
    process
    begin
       wait for 10 us;
    
-      for i in 0 to 7 loop
-         spi_mosi <= dta(7);
-         dta <= dta(6 downto 0) & '0';
-         wait for 1 us;
-         spi_clk <= '1';
-         wait for 1 us;
-         spi_clk <= '0';
-      end loop;
-
+      bus_write(X"81",spi_clk, spi_mosi); wait for 2 us;
+      bus_write(X"11",spi_clk, spi_mosi); wait for 2 us;
+      bus_write(X"AB",spi_clk, spi_mosi); wait for 2 us;
+      bus_write(X"00",spi_clk, spi_mosi); wait for 2 us;
       report "end of simulation";
       
       wait;
